5722d0a816aa12f58d26f5985f29d0903170be4e
NKOKKILIGADDA87 <nagendra.kokiligadda@wipro.com>
2024-12-21 09:30:38 +0530

    sessionStorageEncKey value emity

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -57,7 +57,7 @@ const ADMIN_API = `${adminIP}/adminapi-v3.0.0`;
 const IOT_API = 'http://localhost:8085/ezdx-hub-connect-srv';
 const FHIR_API = `${FHIRIP}/fhirapi-v1.2/`;
 const mmuUICasesheet = `${tmUI_IP}tmui-v3.0.0`;
-const sessionStorageEncKey = '<>';
+const sessionStorageEncKey = '';
 
 export const environment = {
   production: true,
