367b3aa36674aacee66aa0a3de095895fea684c1
srinuatpiramal <srinivas.tirunahari@piramalswasthya.org>
2025-04-23 16:34:33 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -471,8 +471,7 @@ export const environment = {
 
   /** Previous Anthropometry  Urls */
   getPreviousAnthropometryUrl: `${TM_API}anthropometryVitals/getBenHeightDetailsFrmNurse`,
-  /* Customization APIs*/
-  getAllRegistrationData: `${COMMON_API}customization/fetchAllData`,
+  
 
   /*Family Tagging Urls */
   relationShipUrl: `${TM_API}registrar/registrarMasterData`,
