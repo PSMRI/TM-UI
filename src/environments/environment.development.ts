2de8f615b41b31eb7f07f68b80a15b14ccdcbbad
Srinivas Tirunahari <srinivas_t@piramalfndn.dc>
2025-04-24 14:34:49 +0530

    fix(auth): adding envs

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -493,14 +493,7 @@ export const environment = {
   getAbdmMappedFacility: `${COMMON_API}facility/getWorklocationMappedAbdmFacility/`,
   saveAbdmFacilityIdForVisit: `${FHIR_API}facility/saveAbdmFacilityId`,
 
-  // Customization APIs
-  getAllRegistrationData: `${COMMON_API}customization/fetchAllData`,
   getBenIdForhealthID: `${FHIR_API}healthID/getBenIdForhealthID`,
-
-  // Customization APIs
-  getAllRegistrationData: `${COMMON_API}customization/fetchAllData`,
-  getBenIdForhealthID: `${FHIR_API}healthID/getBenIdForhealthID`,
-
 
   /* Abha V3 APIs */
   requestOtpForAbhaEnroll: `${FHIR_API}abhaCreation/requestOtpForAbhaEnrollment`,
