d92668628c6f2489e3a0b7d799ca18df080366ae
vishwab1 <vishwanath@navadhiti.com>
2026-04-02 11:41:47 +0530

    fix: view file

--- src/app/app-modules/nurse-doctor/case-record/general-case-record/test-and-radiology/test-and-radiology.component.ts
+++ src/app/app-modules/nurse-doctor/case-record/general-case-record/test-and-radiology/test-and-radiology.component.ts
@@ -506,8 +506,13 @@ export class TestAndRadiologyComponent implements OnInit, OnDestroy, DoCheck {
         this.labService.viewFileContent(fileID).subscribe(
           (res: any) => {
             if (res.data.statusCode === 200) {
-              const fileContent = res.data.data.response;
-              location.href = fileContent;
+              const fileContent = res.data.data.response?.replace(/^(https?:\/\/)+/, '$1');
+              const a = document.createElement('a');
+              a.href = fileContent;
+              a.target = '_blank';
+              document.body.appendChild(a);
+              a.click();
+              document.body.removeChild(a);
             }
           },
           (err) => {
