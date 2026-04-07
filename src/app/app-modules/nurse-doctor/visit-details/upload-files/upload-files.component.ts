95c1044ebfad12a1c341f5704d079af887152d10
vanitha1822 <vanitha@navadhiti.com>
2026-04-07 22:10:55 +0530

    fix: download issue

--- src/app/app-modules/nurse-doctor/visit-details/upload-files/upload-files.component.ts
+++ src/app/app-modules/nurse-doctor/visit-details/upload-files/upload-files.component.ts
@@ -318,12 +318,24 @@ export class UploadFilesComponent implements OnInit, DoCheck, OnChanges {
         this.labService.viewFileContent(fileID).subscribe((res: any) => {
           if (res && res.statusCode === 200) {
             const fileContent = res.data?.response;
-            const a = document.createElement('a');
-            a.href = fileContent;
-            a.target = '_blank';
-            document.body.appendChild(a);
-            a.click();
-            document.body.removeChild(a);
+            if (fileContent) {
+              const byteCharacters = atob(fileContent);
+              const byteNumbers = new Array(byteCharacters.length);
+              for (let i = 0; i < byteCharacters.length; i++) {
+                byteNumbers[i] = byteCharacters.charCodeAt(i);
+              }
+              const byteArray = new Uint8Array(byteNumbers);
+              const blob = new Blob([byteArray], {
+                type: 'application/octet-stream',
+              });
+              const fileURL = URL.createObjectURL(blob);
+              const a = document.createElement('a');
+              a.href = fileURL;
+              a.target = '_blank';
+              document.body.appendChild(a);
+              a.click();
+              document.body.removeChild(a);
+            }
           }
         });
       }
